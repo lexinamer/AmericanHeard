d6bf3680d4b6a90166b278009e6f9532fc42768e
Maegan Clawges <mclawges22@gmail.com>
2017-01-19 06:12:49 -0800

    Data is hooked up throughout the UI, plus refined 404 page and various styles.

--- public/83746274/javascript/ui.js
+++ public/83746274/javascript/ui.js
@@ -8,7 +8,7 @@ return false;
 
 // Fixed nav bar on mainbody
 var $navBar = $('nav');
-var navPos = $navBar.offset().top - 80;
+var navPos = $navBar.offset().top - 100;
 
 $(window).scroll(function() {
    var scrollPos = $(this).scrollTop();
@@ -20,9 +20,62 @@ $(window).scroll(function() {
    }
 });
 
-$(".current-districts").click(function(event){
-  showFilmCards([event.target.id]);
-});
+function mapClick() {
+  var id = this.id;
+  var stateNum = parseInt(Math.floor(id / 100));
+  var districtNum = id % 100;
+  var filmList = [activeData[id]];
+  for (var i = 0; i < extraData.length; i++) {
+    if (parseInt(extraData[i]["State Number"]) == stateNum) {
+      if (parseFloat(extraData[i]["District Number"]) < 1) {
+        filmList.push(extraData[i]);
+      } else if (math.Floor(parseFloat(extraData[i]["District Number"])) == districtNum) {
+        filmList.push(extraData[i]);
+      }
+    }
+  }
+  showFilmCards(filmList);
+  $('html, body').animate({
+    scrollTop: $("#videolist").offset().top - 80
+  }, 1000);
+  var categoriesSelected = document.getElementsByClassName('category');
+  for (var c = 0; c < categoriesSelected.length; c++) {
+    categoriesSelected[c].classList.remove('selected');
+  }
+}
+
+function toggleCategory(e) {
+  if (e.classList.contains('selected')) {
+    e.classList.remove('selected');
+  } else {
+    e.classList.add('selected');
+  }
+}
+
+function activateFilter() {
+  var categoryElements = document.getElementsByClassName('selected');
+  var key = '';
+  var filmList = [];
+  for (district in activeData) {
+    for (var i = 0; i < categoryElements.length; i++) {
+      key = categoryMap[categoryElements[i].id];
+      if (activeData[district][key] == 'yes') {
+        filmList.push(activeData[district]);
+        break;
+      }
+    }
+  }
+  for (var j = 0; j < extraData.length; j++) {
+    for (var i = 0; i < categoryElements.length; i++) {
+      key = categoryMap[categoryElements[i].id];
+      if (extraData[j][key] == 'yes') {
+        filmList.push(extraData[j]);
+        break;
+      }
+    }
+  }
+  showFilmCards(filmList);
+}
 
 // Filter Bar Styling
 $('select').each(function(){
@@ -61,7 +114,6 @@ $('select').each(function(){
         $styledSelect.text($(this).text()).removeClass('active');
         $this.val($(this).attr('rel'));
         $list.hide();
-        //console.log($this.val());
     });
 
     $(document).click(function() {
